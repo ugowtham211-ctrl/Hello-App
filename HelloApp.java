f3c764017c0c57a84fbcca49fe9b65011c676d57
ugowtham211-ctrl <ugowtham211@gmail.com>
2026-04-20 18:55:41 +0530

    uc2

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,8 @@
-public class HelloApp {
-   public static void main(String[] args) {
-   System.out.println("Hello World");
+@@ -0,0 +1,7 @@
+public class hello {
+    public static void main(String[] args) {
+        // Get the name from command-line argument
+        String name = args[0];
+        System.out.println("Hello, " + name + "!");
     }
-
 }
